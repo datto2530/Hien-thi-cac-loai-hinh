4f38b84e3259a5e7c5f4dfe9ee259a57b6e6ead6
Dattobias <152405844+datto2530@users.noreply.github.com>
2023-12-15 16:22:47 +0700

    chua lam duoc

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int choice; 
+            string i = "*";
+            Console.WriteLine("Menu");
+            Console.WriteLine("1. Ve hinh chu nhat");
+            Console.WriteLine("2. Ve hinh tam giac vuong (top-right)");
+            Console.WriteLine("3. Ve hinh tam giac vuong (top-left)");
+            Console.WriteLine("4. Ve hinh tam giac vuong (bottom-left)");
+            Console.WriteLine("5. Ve hinh tam giac vuong (bottom-right)");
+            Console.WriteLine("6. Ve hinh tam giac can");
+            Console.WriteLine("7. Thoat");
+            Console.WriteLine("Nhap lua chon cua ban: ");
+            choice = Int32.Parse(Console.ReadLine());
+            while (choice < 1 || choice > 7)
+            {
+                Console.WriteLine("Vui long nhap lai lua chon cua ban (tu 1 den 7): ");
+                choice = Int32.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("Lua chon cua ban la: " + choice);
+            switch (choice)
+            {
+                case 1:
+                    Console.WriteLine("Ve hinh chu nhat");
+                    break;
+                case 2:
+                    Console.WriteLine("Ve hinh tam giac vuong (top-right)");
+                    break;
+                case 3:
+                    Console.WriteLine("Ve hinh tam giac vuong (top-left)");
+                    break;
+                case 4:
+                    Console.WriteLine("Ve hinh tam giac vuong (bottom-left)");
+                    break;
+                case 5:
+                    Console.WriteLine("Ve hinh tam giac vuong (bottom-right)");
+                    break;
+                case 6:
+                    Console.WriteLine("Ve hinh tam giac can");
+                    break;
+                case 7:
+                    Environment.Exit(0);
+                    break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Hien thi cac loai hinh/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Hien_thi_cac_loai_hinh
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int Choice = -1;
+            while (Choice != 0)
+            {
+                Console.WriteLine("Menu");
+                Console.WriteLine("1. Ve hinh chu nhat");
+                Console.WriteLine("2. Ve hinh tam giac vuong");
+                Console.WriteLine("3. Ve hinh tam giac can");
+                Console.WriteLine("0. Ket thuc");
+                Console.WriteLine("Nhap lua chon cua ban");
+
+                Choice = int.Parse(Console.ReadLine());
+                switch (Choice)
+                {
+                    case 1:
+                        Console.WriteLine("*********");
+                        Console.WriteLine("********");
+                        Console.WriteLine("*******");
+                        Console.WriteLine("******");
+                        Console.WriteLine("*****");
+                        Console.WriteLine("****");
+                        Console.WriteLine("***");
+                        Console.WriteLine("**");
+                        Console.WriteLine("*");
+                        break;
+                    case 2:
+                        Console.WriteLine("**********");
+                        Console.WriteLine("**********");
+                        Console.WriteLine("**********");
+                        Console.WriteLine("**********");
+                        Console.WriteLine("**********");
+                        Console.WriteLine("**********");
+                        break;
+                    case 3:
+                        Console.WriteLine("**********");
+                        Console.WriteLine("**********");
+                        Console.WriteLine("**********");
+                        break;
+                    case 0:
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("nhap lai");
+                        break;
+                }
+                Console.ReadKey();
+            }
+    }
+}
